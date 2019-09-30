d3641b2007c6ab0cabf8877a29150662da5e64f7
ruankenedi <ruankenedi01@gmail.com>
2019-09-30 17:57:25 -0300

    My layout default

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -19,7 +19,7 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer
+    <!-- <q-drawer
       v-model="leftDrawerOpen"
       show-if-above
       bordered
@@ -32,7 +32,7 @@
             <q-icon name="school" />
           </q-item-section>
           <q-item-section>
-            <q-item-label>Docs</q-item-label>
+            <q-item-label>Categoria</q-item-label>
             <q-item-label caption>quasar.dev</q-item-label>
           </q-item-section>
         </q-item>
@@ -81,6 +81,107 @@
             <q-item-label caption>@QuasarFramework</q-item-label>
           </q-item-section>
         </q-item>
+      </q-list>
+    </q-drawer> -->
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      bordered
+      content-class="bg-grey-2"
+    >
+      <q-list bordered class="rounded-borders">
+        <q-expansion-item
+          expand-separator
+          icon="mail"
+          label="Categorias"
+          caption="2 opções"
+        >
+          <q-expansion-item
+            :header-inset-level="1"
+            expand-separator
+            icon="add_circle"
+            label="Criar"
+            :to="{ name: 'createCategory' }"
+          >
+            <q-card>
+              <q-card-section>
+                Campo para a criação de categoria!
+              </q-card-section>
+            </q-card>
+          </q-expansion-item>
+
+          <q-expansion-item
+            :header-inset-level="1"
+            expand-separator
+            icon="settings"
+            label="Gerenciar Categorias"
+            :to="{ name: 'settingsCategory' }"
+          >
+            <q-card>
+              <q-card-section>
+                Editar, excluir e administração das categorias!
+              </q-card-section>
+            </q-card>
+          </q-expansion-item>
+        </q-expansion-item>
+
+        <q-expansion-item
+          expand-separator
+          icon="mail"
+          label="Produtos"
+          caption="2 opções"
+        >
+          <q-expansion-item
+            :header-inset-level="1"
+            expand-separator
+            icon="add_circle"
+            label="Criar"
+            :to="{ name: 'createProduct' }"
+          >
+            <q-card>
+              <q-card-section>
+                Campo para criar produtos!
+              </q-card-section>
+            </q-card>
+          </q-expansion-item>
+
+          <q-expansion-item
+            :header-inset-level="1"
+            expand-separator
+            icon="settings"
+            label="Gerenciar Produtos"
+            :to="{ name: 'settingsProduct' }"
+          >
+            <q-card>
+              <q-card-section>
+                Editar, excluir e administração dos produtos!
+              </q-card-section>
+            </q-card>
+          </q-expansion-item>
+
+        </q-expansion-item>
+
+        <!-- <q-expansion-item
+          :content-inset-level="0.5"
+          expand-separator
+          icon="mail"
+          label="Usuários"
+          caption="5 opções"
+        >
+          <q-expansion-item
+            :content-inset-level="0.5"
+            expand-separator
+            icon="settings"
+            label="Gerenciar Usuários"
+            :to="{ name: 'settingsUser' }"
+          >
+            <q-card>
+              <q-card-section>
+                Editar, excluir e administração das permissões e dos status!
+              </q-card-section>
+            </q-card>
+          </q-expansion-item>
+        </q-expansion-item> -->
       </q-list>
     </q-drawer>
 
