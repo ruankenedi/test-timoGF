027db9eaeab1fb4702a2967c0e7501653be8320c
ruankenedi <ruankenedi01@gmail.com>
2019-10-02 03:53:40 -0300

    Add route error

--- src/router/routes.js
+++ src/router/routes.js
@@ -33,12 +33,12 @@ const routes = [
   }
 ]
 
-// // Always leave this as last one
-// if (process.env.MODE !== 'ssr') {
-//   routes.push({
-//     path: '*',
-//     component: () => import('pages/Error404.vue')
-//   })
-// }
+// Always leave this as last one
+if (process.env.MODE !== 'ssr') {
+  routes.push({
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  })
+}
 
 export default routes
